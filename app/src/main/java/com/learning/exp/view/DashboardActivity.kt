ca5c7082adcfd7cf1fc53f0748ff70e9866e3127
greatapp <groomrent@gmail.com>
2026-05-23 21:19:04 +0530

    App flow completed

---
+++ app/src/main/java/com/learning/exp/view/DashboardActivity.kt
@@ -0,0 +1,28 @@
+package com.learning.exp.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.learning.exp.databinding.DashboardActivityBinding
+
+class DashboardActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "DashboardActivity"
+    }
+
+
+    private lateinit var mBinding: DashboardActivityBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = DashboardActivityBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.lehengaFL.setOnClickListener {
+            startActivity(Intent(this, LahangaListActivity::class.java))
+        }
+
+        mBinding.sherwaniFl.setOnClickListener {
+        //    startActivity(Intent(this, LahangaListActivity::class.java))
+        }
+    }
+}
